96eb3051c8c7b03b572f4c4af6fa3decc263cbc3
mh4g <103978552+mh4g@users.noreply.github.com>
2025-01-09 06:20:37 +0900

    Delete Docker 練習 Python directory

--- Docker 練習 Python/src/test.py
+++
@@ -1,7 +0,0 @@
-# output.txt に書き込む
-file_path = "/workspace/output.txt"  # コンテナ内でのパス
-with open(file_path, "w") as file:
-    file.write("これはDocker内で書き込まれたテキストです。")
-print("ファイルに書き込みました:", file_path)
-
-
